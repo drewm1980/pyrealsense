65712c7da767e2e041cdd7ad777d91ba036458a7
Andrew Wagner <andrew.wagner@robovision.eu>
2017-02-14 15:20:08 +0100

    Improve librealsense library search path

--- pyrealsense/importlib.py
+++ pyrealsense/importlib.py
@@ -1,29 +1,24 @@
 import ctypes
 import sys
 import os
+from pathlib import Path
 
-os_name = sys.platform
-lrs_suffix_mapping = {'darwin':'.dylib', 'linux':'.so', 'linux2':'.so'} # 'win':'.dll'}
-rsu_suffix_mapping = {'darwin':'.so', 'linux':'.so', 'linux2':'.so'} # 'win':'.dll'}
+if sys.platform in ('linux','linux2'):
+    soname = 'librealsense.so'
+    search_paths = [
+        '.', '/usr/local/lib', '/usr/local/lib/x86_64-linux-gnu', '/usr/lib'
+    ]
+elif sys.platform == 'darwin':
+    soname = 'librealsense.dylib'
+else:
+    assert False, 'Please add library search paths for your platform to importlib.py'
 
-try:
-    lrs_suffix = lrs_suffix_mapping[os_name]
-    rsu_suffix = rsu_suffix_mapping[os_name]
-except KeyError:
-    raise OSError('OS not supported.')
-
-## hacky way to load "extension" module
-def _find_extension_name():
-    dirname = os.path.dirname(__file__)
-
-    for f in os.listdir(dirname):
-        if f.endswith(rsu_suffix):
-            f_name = f
-
-    return os.path.join(dirname, f_name)
-
-rsutilwrapper = ctypes.CDLL(_find_extension_name())
+file_path = None
+for path in search_paths:
+    if (Path(path) / soname).is_file():
+        file_path = Path(path) / soname
+        break
+assert file_path is not None, "Could not find the library file " + soname + 'in any of the directories '+str(search_paths)
 
 ## import C lib
-lrs = ctypes.CDLL('librealsense'+lrs_suffix)
-
+lrs = rsutilwrapper = ctypes.CDLL(str(file_path))
